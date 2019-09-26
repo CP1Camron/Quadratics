6981c6d5ba0cc88d2dd5c7e0aa6e3e0177455500
Camron Caldwell <camron.caldwell.2022@student.lhschools.org>
2019-09-26 09:58:49 -0400

    declared variables

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        
+
         //todo declare variables a,b,c,discriminant, axisOfSymmetry,parabolaFacing, x,y
 
         //todo initialize manually
